d742fc91a0b203fcb7c511f95e99ba18165a1732
Jasmine Daniels <create.jasminedaniels@gmail.com>
2022-09-18 14:31:25 -0400

    server starter files

---
+++ server/utils/seed.js
@@ -0,0 +1,8 @@
+// TODO: import connection
+// TODO: import models
+// TODO: import tarot-card-data
+
+
+//TODO: Card.collection.insertMany(cardData)
+//TODO: Deck.collection.insertMany(Cards)
+//TODO: User.collection.insertMany(users)
