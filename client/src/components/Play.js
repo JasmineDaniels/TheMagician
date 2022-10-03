d600b623114ff53aabd97de0f3b377e29f842f8f
Jasmine Daniels <create.jasminedaniels@gmail.com>
2022-10-03 15:05:16 -0400

    card alignment

--- client/src/components/Play.js
+++ client/src/components/Play.js
@@ -1,9 +1,10 @@
 import Card from "./Card"
+import cards from '../utils/cards.json'
 export default function Play (){
     return (
         <div>
             <h1 id="PLAY" className="text-center">PLAY</h1>
-            <Card/>
+            <Card cards={cards}/>
         </div>
     )
 }
