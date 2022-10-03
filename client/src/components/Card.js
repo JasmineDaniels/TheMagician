c11d694d0120ad9aac4992660c81c6febd56b350
Jasmine Daniels <create.jasminedaniels@gmail.com>
2022-10-03 17:51:09 -0400

    card flip event -wip

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -8,35 +8,49 @@ import m01 from '../images/m01.jpg'
 
 
 export default function Card ({ cards }){
-    let [flip, setFlip] = useState('false')
+    const [flip, setFlip] = useState('false')
+    //let state = { flip: false }
 
-    const handleClick = () => {
-        setFlip(!flip);
+    // const handleClick = (e) => {
+    //     const { target } = e;
+    //     const id = target.id;
+    //     console.log(`Card ${id} was clicked`)
+    //     if (id){
+    //         setFlip(!flip);
+    //     }
+        
+    //     //axios get card = card.img
+    //     //axios post card back + info to results  
+    // }
 
-        //axios get card = card.img
-        //axios post card back + info to results  
+    function handleClick () {
+        setFlip(!flip)
     }
 
+    // function handleClick(){
+    //     this.setSate({ flip: !this.state.flip })
+    // }
+    // const flip = this.state.flip;
     return (
         <Container>
             <Row >
                 {/* <Col md='3'> */}
-                    {cards.flatMap((card) => (
-                        <Col md={2} id={card.number}>
-                            {/* {cards.map((card) => ( */}
-                                <div className='card'>
-                                    <div className={flip ? "card__inner" : 'card__inner is-flipped'} >
-                                        <div className='card__face card__face--front'>
-                                            <img src={m01} alt='card-back' className='card-fit' onClick={handleClick}></img>
-                                        </div>
-                                        <div className='card__face card__face--back'>
-                                            <h6>{card.name}</h6>
-                                            <img src={require(`../images/${card.img}`)} alt='card-back' className='card-fit'></img>
-                                        </div>
-
+                    {cards.flatMap((card, index) => (
+                        <Col md={2} id={card.number} key={index}>
+                            
+                            <div className='card'>
+                                <div className={flip ? "card__inner" : 'card__inner is-flipped'} >
+                                    <div className='card__face card__face--front'>
+                                        <img src={m01} alt='card-front' className='card-fit' onClick={handleClick}></img>
                                     </div>
+                                    <div className='card__face card__face--back'>
+                                        <h6>{card.name}</h6>
+                                        <img src={require(`../images/${card.img}`)} alt='card-back' className='card-fit'></img>
+                                    </div>
+
                                 </div>
-                            {/* ))} */}
+                            </div>
+                            
                             
                         </Col>
                     ))}
