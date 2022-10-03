85ff0e68e2e03cf46d733a69f4d603888721ba7c
Jasmine Daniels <create.jasminedaniels@gmail.com>
2022-10-03 15:22:52 -0400

    card back connected

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -22,7 +22,7 @@ export default function Card ({ cards }){
             <Row >
                 {/* <Col md='3'> */}
                     {cards.flatMap((card) => (
-                        <Col md='3' id={card.number}>
+                        <Col md={2} id={card.number}>
                             {/* {cards.map((card) => ( */}
                                 <div className='card'>
                                     <div className={flip ? "card__inner" : 'card__inner is-flipped'} >
@@ -31,7 +31,7 @@ export default function Card ({ cards }){
                                         </div>
                                         <div className='card__face card__face--back'>
                                             <h6>{card.name}</h6>
-                                            <img src={card.img} alt='card-back' className='card-fit'></img>
+                                            <img src={require(`../images/${card.img}`)} alt='card-back' className='card-fit'></img>
                                         </div>
 
                                     </div>
