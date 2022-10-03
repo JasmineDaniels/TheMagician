1924d620b238a9f4747d1cf3e43db9543042174a
Jasmine Daniels <create.jasminedaniels@gmail.com>
2022-10-03 13:01:08 -0400

    card template v1.0

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,15 +1,36 @@
 import '../css/card.css'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import c04 from '../images/c04.jpg'
+import m01 from '../images/m01.jpg'
 export default function Card (){
     return (
-        <div className='container'>
-            <div className='card mx-auto'>
-                <div className='card__inner'>
-                    <div className='card__face card__face--front'></div>
-                    <div className='card__face card__face--back'></div>
+        <Container>
+            <Row >
+                <Col md='4'>
+                    <div className='card '>
+                        <div className='card__inner'>
+                            <div className='card__face card__face--front'>
+                                
+                                <img src={m01} alt='card-back' className='card-fit'></img>
+                            </div>
+                            <div className='card__face card__face--back'>
+                                
+                                <img src={c04} alt='card-back' className='card-fit'></img>
+                            </div>
 
-                </div>
+                        </div>
+                    </div>
+                </Col>
+                
+
+            </Row>
+            
+            <div>
+                Results
             </div>
-        </div>
-        
+        </Container>
+    
     )
 }
--- client/src/components/Play.js
+++ client/src/components/Play.js
@@ -1,7 +1,9 @@
+import Card from "./Card"
 export default function Play (){
     return (
         <div>
-            <h1 id="PLAY">PLAY</h1>
+            <h1 id="PLAY" className="text-center">PLAY</h1>
+            <Card/>
         </div>
     )
 }
