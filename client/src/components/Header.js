2a5517e85f7d47d87c71ae2df953f5cc58251950
Jasmine Daniels <create.jasminedaniels@gmail.com>
2022-10-14 15:11:48 -0400

    backgroung img + animation

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,16 @@
 import '../css/header.css'
 function Header (){
     return (
-        <header className='container-fluid'>
-            <div className='row nav-item '>
+        <header className='container-fluid '>
+            <div className='row nav-item'>
+                
                 <div className='col-md-8 mx-auto text-center'>
                     <div className='header-width mx-auto'>
-                        <a className='navbar-brand header-bg header-title nav-link active' href="/"> THE MAGICIAN </a>
+                        <a className='navbar-brand header-bg header-title key nav-link active' href="/"> <span>THE</span> <span>MAGICIAN</span> </a>
                     </div>
                 </div>
+                
+                
             </div>
         </header>
     )
