0a257833ac379b1a25c0c9f729ca16df3b110dc4
Jasmine Daniels <create.jasminedaniels@gmail.com>
2022-11-22 18:48:55 -0500

    fix eslint error

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,15 +2,15 @@ const { connect, connection } = require('mongoose');
 require('dotenv').config()
 
 
-connect( process.env.MONGODB_URI || `mongodb://127.0.0.1:27017/theMagicianDB`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-// connect( process.env.MONGODB_URI , {
+// connect( process.env.MONGODB_URI || `mongodb://127.0.0.1:27017/theMagicianDB`, {
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true,
 // });
+
+connect( process.env.MONGODB_URI , {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 
 module.exports = connection;
